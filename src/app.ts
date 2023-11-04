2cba73dcb92624d0946337d10a147ced0bd264e2
rafeulanamudoy <rafeuludoy@gmail.com>
2023-11-05 01:33:13 +0600

    globalError handler middleware add to handle errors

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,12 @@ import express, { Application, Request, Response } from "express";
 const app: Application = express();
 import cors from "cors";
 import { routes } from "./app/routes";
+import globalErrorHandler from "./app/middlewares/globalErroHandler";
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1", routes);
-
+app.use(globalErrorHandler);
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
